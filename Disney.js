14d7d7569766fda4f40401792b67b5f4baa3362d
AparnaTiwarii <tiwari.ms.aparna23076@jeckukas.org.in>
2025-06-17 18:18:52 +0530

    Update Disney.js

--- Disney.js
+++ Disney.js
@@ -26,11 +26,8 @@ let movies=[
     },
 ]
 
-
 const carousel = document.querySelector(".carousel");
-
 let sliders = [];
-
 let slideIndex = 0;
 
 const createSlide = () => {
@@ -38,53 +35,47 @@ const createSlide = () => {
         slideIndex = 0;
     }
 
+    let slide = document.createElement("div");
+    var imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
 
-// create Dom Element
+    imgElement.appendChild(document.createTextNode(""));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
 
-let slide = document.createElement("div");
-var imgElement = document.createElement("img");
-let content = document.createElement("div");
-let h1 = document.createElement("h1");
-let p = document.createElement("p");
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
 
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    p.className = "movie-des";
 
-imgElement.appendChild(document.createTextNode(""));
-h1.appendChild(document.createTextNode(movies[slideIndex].name));
-p.appendChild(document.createTextNode(movies[slideIndex].des));
-content.appendChild(h1);
-content.appendChild(p);
-slide.appendChild(content);
-slide.appendChild(imgElement);
-carousel.appendChild(slide);
+    sliders.push(slide);
 
-
-imgElement.src = movies[slideIndex].image;
-slideIndex++;
-
-slide.className = "slider";
-content.className = "slide-content";
-h1.className = "movie-title";
-p.className = "movie-des";
-
-sliders.push(slide);
-
-if (sliders.length) {
-    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${ 
-        30 * (sliders.length - 2)
-    }px)`;
-  }
+    if (sliders.length) {
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${ 
+            30 * (sliders.length - 2)
+        }px)`;
+    }
 };
 
 for(let i = 0; i < 3; i++){
     createSlide();
-};
+}
 
 setInterval(()=>{
     createSlide();
 }, 3000);
 
 //video cards
-
 const videoCards = [...document.querySelectorAll(".video-card")];
 videoCards.forEach(item => {
     item.addEventListener("mouseover", () => {
@@ -98,7 +89,6 @@ videoCards.forEach(item => {
 });
 
 // card sliders
-
 let cardContainers = [...document.querySelectorAll(".card-container")];
 let preBtns = [...document.querySelectorAll(".pre-btn")];
 let nxtBtn = [...document.querySelectorAll(".nxt-btn")];
@@ -115,44 +105,3 @@ cardContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth + 200;
     });
 });
-
-@media (max-width: 768px) {
-  .navbar {
-    padding: 0 2%;
-  }
-
-  .search-box {
-    width: 150px;
-  }
-
-  .search-box:focus {
-    width: 200px;
-  }
-
-  .video-card-container {
-    flex-wrap: wrap;
-  }
-
-  .video-card {
-    min-width: calc(100% / 2 - 10px);
-    margin-bottom: 10px;
-  }
-}
-
-@media (max-width: 480px) {
-  .navbar {
-    padding: 0 1%;
-  }
-
-  .search-box {
-    width: 100px;
-  }
-
-  .search-box:focus {
-    width: 150px;
-  }
-
-  .video-card {
-    min-width: 100%;
-  }
-}
