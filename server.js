e4c73bc7d6938e231c2f95d689b8aae5387c2879
AparnaTiwarii <tiwari.ms.aparna23076@jeckukas.org.in>
2025-06-28 12:49:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files (images, videos, etc.) from the root directory
 app.use(express.static(path.join(__dirname)));
+
+// Serve index.html for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
